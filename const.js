1cfa810acf79526731e829928e85f74f732d09f6
seven <devmagento2018@gmail.com>
2022-06-16 00:37:01 +0700

    build app with v1.0.0

--- const.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    host: 'http://service.easygun.com',
-    flashUrl: 'http://flash.gunny.com:88',
-    updaterUrl: ''
-}
-module.exports = config;
---
+++ resources/app/const.js
@@ -0,0 +1,6 @@
+const config = {
+    host: 'http://service.easygun.com',
+    flashUrl: 'http://flash.gunny.com:88',
+    updaterUrl: 'https://easy-gun-launcher-updater-server-3sxwrf2ns-koolkiizz.vercel.app'
+}
+module.exports = config;
